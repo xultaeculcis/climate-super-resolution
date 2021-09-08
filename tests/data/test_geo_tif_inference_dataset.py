03db5f0cf2bb018fa874df1d89cc9c9e93314880
Michal Wierzbinski <michal.wierzbinski95@gmail.com>
2021-09-08 18:58:15 +0200

    Fix GeoTiffInferenceDataset tests

--- tests/data/test_geo_tif_inference_dataset.py
+++ tests/data/test_geo_tif_inference_dataset.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
+import os
+from pathlib import Path
+from typing import Tuple
+
 import pandas as pd
+from pytest_cases import parametrize_with_cases
 
 import climsr.consts as consts
 from climsr.data.sr.geo_tiff_inference_dataset import GeoTiffInferenceDataset
 
-europe_extent_expected_shape_hr = [412, 452]
-europe_extent_expected_shape_lr = [103, 113]
+europe_extent_expected_shape_hr_1d = (1, 412, 452)
+europe_extent_expected_shape_hr_3d = (3, 412, 452)
+europe_extent_expected_shape_lr_1d = (1, 103, 113)
+europe_extent_expected_shape_lr_3d = (3, 103, 113)
 var = "tmp"
-df = pd.read_csv("./datasets/statistics_min_max.csv")
+
+root_dir = str(Path(__file__).parent.parent.parent)
+data_dir = os.path.join(root_dir, "datasets/pre-processed")
+
+df = pd.read_csv(os.path.join(data_dir, "csv/statistics_min_max.csv"))
 df = df[(df[consts.datasets_and_preprocessing.dataset] == "cru-ts") & (df[consts.datasets_and_preprocessing.variable] == var)]
-tiff_dir = f"/media/xultaeculcis/2TB/datasets/cruts/pre-processed/europe-extent/{var}"
-tiff_df = df
-elevation_file = "/media/xultaeculcis/2TB/datasets/cruts/pre-processed/europe-extent/elevation/wc2.1_2.5m_elev.tif"
-land_mask_file = "/media/xultaeculcis/2TB/datasets/cruts/pre-processed/europe-extent/mask/wc2.1_2.5m_prec_1961-01.tif"
+tiff_dir = os.path.join(data_dir, f"cruts/europe-extent/{var}")
+elevation_file = os.path.join(data_dir, "cruts/europe-extent/elevation/wc2.1_2.5m_elev.tif")
+land_mask_file = os.path.join(data_dir, "cruts/europe-extent/mask/wc2.1_2.5m_tmin_1961-01.tif")
 variable = var
 scaling_factor = 4
 normalize = True
@@ -27,7 +37,7 @@ use_mask_as_3rd_channel = True
 def _get_dataset(gen):
     return GeoTiffInferenceDataset(
         tiff_dir=tiff_dir,
-        tiff_df=tiff_df,
+        tiff_df=df,
         elevation_file=elevation_file,
         land_mask_file=land_mask_file,
         generator_type=gen,
@@ -43,55 +53,28 @@ def _get_dataset(gen):
     )
 
 
-def test_should_return_proper_data_when_used_with_srcnn():
+def case_rcan() -> Tuple[str, Tuple[int, int, int]]:
+    return consts.models.rcan, europe_extent_expected_shape_lr_3d
+
+
+def case_srcnn() -> Tuple[str, Tuple[int, int, int]]:
+    return consts.models.srcnn, europe_extent_expected_shape_hr_3d
+
+
+@parametrize_with_cases("generator_name,expected_shape", cases=".")
+def test_should_return_expected_data_shapes_for_each_generator_type(generator_name: str, expected_shape: Tuple[int, int, int]):
     # arrange
-    sut = _get_dataset(consts.models.srcnn)
+    sut = _get_dataset(generator_name)
 
     # act
     out = sut[1]
 
     # assert
-    assert out[consts.batch_items.lr].shape == (3, *europe_extent_expected_shape_hr)
-    assert out[consts.batch_items.elevation].shape == (
-        1,
-        *europe_extent_expected_shape_hr,
-    )
-    assert out[consts.batch_items.elevation_lr].shape == (
-        1,
-        *europe_extent_expected_shape_lr,
-    )
-    assert out[consts.batch_items.nearest].shape == (
-        1,
-        *europe_extent_expected_shape_hr,
-    )
-    assert out[consts.batch_items.mask].shape == (1, *europe_extent_expected_shape_hr)
-    assert out[consts.batch_items.mask_np].shape == tuple(europe_extent_expected_shape_hr)
-    assert out[consts.batch_items.min] is not None
-    assert out[consts.batch_items.max] is not None
-
-
-def test_should_return_proper_data_when_used_not_with_srcnn():
-    # arrange
-    sut = _get_dataset(consts.models.rcan)
-
-    # act
-    out = sut[1]
-
-    # assert
-    assert out[consts.batch_items.lr].shape == (3, *europe_extent_expected_shape_lr)
-    assert out[consts.batch_items.elevation].shape == (
-        1,
-        *europe_extent_expected_shape_hr,
-    )
-    assert out[consts.batch_items.elevation_lr].shape == (
-        1,
-        *europe_extent_expected_shape_lr,
-    )
-    assert out[consts.batch_items.nearest].shape == (
-        1,
-        *europe_extent_expected_shape_hr,
-    )
-    assert out[consts.batch_items.mask].shape == (1, *europe_extent_expected_shape_hr)
-    assert out[consts.batch_items.mask_np].shape == tuple(europe_extent_expected_shape_hr)
+    assert out[consts.batch_items.lr].shape == expected_shape
+    assert out[consts.batch_items.elevation].shape == europe_extent_expected_shape_hr_1d
+    assert out[consts.batch_items.elevation_lr].shape == europe_extent_expected_shape_lr_1d
+    assert out[consts.batch_items.nearest].shape == europe_extent_expected_shape_hr_1d
+    assert out[consts.batch_items.mask].shape == europe_extent_expected_shape_hr_1d
+    assert out[consts.batch_items.mask_np].shape == tuple(europe_extent_expected_shape_hr_1d[1:])
     assert out[consts.batch_items.min] is not None
     assert out[consts.batch_items.max] is not None
